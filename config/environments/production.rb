23dba0e335151f3ea929c666bc32243f01268eea
bogdanRada <raoul_ice@yahoo.com>
2012-06-11 12:51:06 +0300

    production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,7 +26,7 @@ Fullcalendarlatest2::Application.configure do
   # Specifies the header that your server uses for sending files
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
-
+  config.action_dispatch.x_sendfile_header = "X-Sendfile"
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
@@ -44,7 +44,7 @@ Fullcalendarlatest2::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
-
+ config.assets.precompile += ['*.js', '*.css']
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
 
